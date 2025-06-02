3a3b0b22aff8305d0a5b923a76e6719f8e4e2e2b
DESKTOP-4E9T2MH\Admin <ajoy9532@gmail.com>
2025-06-02 15:56:12 +0600

    ReviewGuardian section complete

--- src/Component/ReviewGuardian/ReviewGuardian.jsx
+++ src/Component/ReviewGuardian/ReviewGuardian.jsx
@@ -1,5 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+
+// import required modules
+import { Autoplay, Pagination } from "swiper/modules";
 
 const ReviewGuardian = () => {
   return (
@@ -26,6 +35,52 @@ const ReviewGuardian = () => {
           <path d="M190.5 66.9l22.2-22.2c9.4-9.4 24.6-9.4 33.9 0L441 239c9.4 9.4 9.4 24.6 0 33.9L246.6 467.3c-9.4 9.4-24.6 9.4-33.9 0l-22.2-22.2c-9.5-9.5-9.3-25 .4-34.3L311.4 296H24c-13.3 0-24-10.7-24-24v-32c0-13.3 10.7-24 24-24h287.4L190.9 101.2c-9.8-9.3-10-24.8-.4-34.3z"></path>
         </svg>
       </Link>
+
+      <div className="mx-auto px-4 lg:max-w-[60rem] xl:max-w-[71.25rem] mb-8 mt-4">
+        <Swiper
+          breakpoints={{
+            0: {
+              slidesPerView: 1.5, // On mobile (0px and up)
+            },
+            768: {
+              slidesPerView: 2.5, // On tablets
+            },
+            1024: {
+              slidesPerView: 3, // On desktops
+            },
+          }}
+          spaceBetween={30}
+          loop={true}
+          autoplay={{
+            delay: 1,
+            disableOnInteraction: false,
+          }}
+          speed={3000}
+          pagination={{
+            clickable: true,
+          }}
+          modules={[Autoplay, Pagination]}
+          className="mySwiper">
+          <SwiperSlide>
+            <img src="/poster1.jpg" alt="" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src="/poster2.jpg" alt="" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src="/poster1.jpg" alt="" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src="/poster2.jpg" alt="" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src="/poster1.jpg" alt="" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src="/poster2.jpg" alt="" />
+          </SwiperSlide>
+        </Swiper>
+      </div>
     </div>
   );
 };
